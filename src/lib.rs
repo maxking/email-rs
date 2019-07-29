9cce8c6ff8db1fe84892efe2dc5ba62052d24a72
Abhilash Raj <raj.abhilash1@gmail.com>
2019-07-28 17:49:47 -0700

    Add the ability parse emails with multi-line headers.

--- src/lib.rs
+++ src/lib.rs
@@ -21,37 +21,55 @@ type EmailBody = String;
 
 
 impl Email {
-    fn from_str(s: &String) -> Email {
+    pub fn from_str(s: String) -> Email {
         let mut allheaders = HashMap::new();
-        let mut email_lines = s.lines();
-        // First, let's try to parse headers in the emails.
-        while true{
-            let line = email_lines.next();
-            match line {
-                // Stop parsing headers as soon as we encounter an empty line.
-                Some("") => break,
-                Some(value) => {
-                    let header: Vec<&str> = value.rsplitn(2, ":").collect();
-                    allheaders.insert(String::from(header[1]), String::from(header[0].trim_start()));
-                },
-                None => break,
-            }
-        };
 
-        let mut body = String::new();
-        loop {
-            let line = email_lines.next();
-            match line {
-                Some(value) => body.push_str(value),
-                None => break
+        let val: Vec<&str> = s.rsplitn(2, "\n\n").collect();
+        let mut headers = Some(val[1]);
+
+
+        while let Some(_) = headers {
+            match Email::get_one_header(headers.unwrap()) {
+                (Some(headerval), rest) => {
+                    let key_val: Vec<&str> = headerval.rsplitn(2, ':').collect();
+                    allheaders.insert(String::from(key_val[1]), String::from(key_val[0].trim_start()));
+                    headers = rest;
+                },
+                _ => break,
             }
         }
 
         Email{
             headers: allheaders,
-            body: body,
+            body: val[0].to_string(),
             children: vec!()
         }
+    }
+
+    /// get_one_header tries to parse one header line from the provided buffer
+    /// and returns the rest back.
+    fn get_one_header(_s: &str) -> (Option<String>, Option<&str>) {
+        let mut header_line = String::new();
+        let mut last = 0;
+        let bytes = _s.as_bytes();
+        for (i, &x) in bytes.iter().enumerate() {
+            last = i;
+            if x == b'\n' {
+                if bytes[i+1] == b' ' {
+                    header_line.push(x as char);
+                } else {
+                    break;
+                }
+            } else {
+                header_line.push(x as char);
+            }
+        }
+
+        let mut rest = Some(&_s[last+1..]);
+        if last+1 == _s.len() {
+            rest = None;
+        }
+        (Some(header_line), rest)
     }
 
     fn from_bytes() -> Email {
@@ -70,7 +88,7 @@ impl Email {
 
 #[cfg(test)]
 #[test]
-fn it_works() {
+fn test_parse_simple_email() {
     let emailstr = String::from("From: maxking@example.com
 To: something@person.com
 Date: 9th Oct 2019
@@ -80,9 +98,60 @@ Message-ID: <messagecanthavespace@localhost.localdomain>
 Hello World.
 ");
 
-    let email = Email::from_str(&emailstr);
-    for (header, value) in &email.headers {
-        println!("key = {:?}, value = {:?}", header, value);
-    }
-    assert_eq!(email.body, String::from("Hello World."))
+    let email = Email::from_str(emailstr);
+    assert_eq!(email.body, String::from("Hello World.\n"));
+    println!("{:?}", email.headers.keys());
+    assert_eq!(email.headers.len(), 5);
+    assert_eq!(email.headers.get("To"), Some(&"something@person.com".to_string()));
+    assert_eq!(email.headers.get("Date"), Some(&"9th Oct 2019".to_string()));
+    assert_eq!(email.headers.get("Subject"), Some(&"This is current email's subject".to_string()));
+    assert_eq!(email.headers.get("From"), Some(&"maxking@example.com".to_string()));
+    assert_eq!(email.headers.get("Message-ID"), Some(&"<messagecanthavespace@localhost.localdomain>".to_string()));
+
 }
+
+#[test]
+fn test_parse_emails_with_multiline_headers() {
+    let emailstr = "From: maxking
+To: Someone
+Subject: This is a multiline subject
+ which goes on for a while because I chose
+ to fold it.
+
+This is the body of the email".to_string();
+    let email = Email::from_str(emailstr);
+    assert_eq!(email.body, "This is the body of the email".to_string());
+    assert_eq!(email.headers.len(), 3);
+    assert_eq!(email.headers.get("To"), Some(&"Someone".to_string()));
+    assert_eq!(email.headers.get("From"), Some(&"maxking".to_string()));
+    assert_eq!(email.headers.get("Subject"),
+               Some(&"This is a multiline subject\n which goes on for a while because I chose\n to fold it.".to_string()));
+
+
+}
+
+
+#[test]
+fn test_get_one_simple_header() {
+    let headers = "From: Someone
+To: Person
+Date: Today";
+    assert_eq!(Email::get_one_header(headers),
+               (Some("From: Someone".to_string()), Some("To: Person\nDate: Today")))
+}
+
+    #[test]
+    fn test_get_one_multiline_header() {
+        let headers = "From: acomplexheader
+Subject: This is a complex header which goes to
+ 2nd line identified by whitespace at the
+ start of each next line of header.";
+        let (header, rest) = Email::get_one_header(headers);
+        assert_eq!(header, Some("From: acomplexheader".to_string()));
+        assert_eq!(rest.is_some(), true);
+        let (header, rest) = Email::get_one_header(rest.unwrap());
+        assert_eq!(header, Some("Subject: This is a complex \
+                                 header which goes to\n 2nd line identified by whitespace at \
+                                 the\n start of each next line of header.".to_string()));
+        assert_eq!(rest, None);
+    }
